3108ccc7cd8bee913c8ce9af8dfebfbd40fd37ef
Charlie Appleton <xjarlie@gmail.com>
2022-03-17 17:45:12 +0900

    stuf n things

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ app.set('view engine', 'ejs');
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.json());
 
+const animals = {
+    africanElephant: 'African Elephant', 
+    asianElephant: 'Asian Elephant',
+    blueWildebeest: 'Blue Wildebeest', 
+    africanElephantNewborn: 'Newborn African Elephant',
+    asianElephantNewborn: 'Newborn Asian Elephant'
+}
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { animals });
 });
 
 app.listen(port, (req, res) => {
